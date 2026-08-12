73c3f067b17e29021e7bde0559a60ca7d6b34e3f
Juan <juanhr1424@gmail.com>
2026-08-12 18:59:40 -0500

    first commit

---
+++ app/Models/Usuario.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+
+    protected $table = 'usuario';
+
+    public $timestamps = false;
+
+    protected $primaryKey = 'id_usuario';
+
+    //use HasFactory;
+}
